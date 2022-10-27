93d281204f77076a61866b200daf2712a16d1444
yrhiba <yrhiba@student.1337.ma>
2022-10-27 22:16:16 +0100

    modified:   ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 01:27:27 by yrhiba            #+#    #+#             */
-/*   Updated: 2022/10/27 18:46:52 by yrhiba           ###   ########.fr       */
+/*   Updated: 2022/10/27 22:15:19 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef struct s_print
 	int		m_num;
 }			t_print;
 
-int			ft_printf(const char *, ...);
+int			ft_printf(const char *format, ...);
 void		ft_init_tab(t_print *tab);
 int			ft_check_flags(const char *format, int index, t_print *to_print);
 int			ft_atoi(const char *str);
