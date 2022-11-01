50a0039fee6751b143332208fece914682ecfc46
yrhiba <yahyarhiba@gmail.com>
2022-11-01 02:22:37 +0100

    add flag + to format_%d

--- ft_format_d.c
+++ ft_format_d.c
@@ -6,22 +6,50 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/31 16:38:18 by yrhiba            #+#    #+#             */
-/*   Updated: 2022/11/01 01:56:21 by yrhiba           ###   ########.fr       */
+/*   Updated: 2022/11/01 02:22:20 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+char	*ft_format_plus(char *nbr)
+{
+	char	*rtn;
+	size_t	size;
+	size_t	i;
+
+	if (nbr[0] == '-')
+		return (nbr);
+	size = ft_strlen(nbr) + 2;
+	rtn = (char *)malloc(sizeof(char) * size);
+	if (!rtn)
+		return (free(nbr), NULL);
+	i = 0;
+	rtn[i] = '+';
+	while (++i < size - 1)
+		rtn[i] = nbr[i - 1];
+	return (rtn[i] = '\0', free(nbr), rtn);
+}
+
+char	*ft_format_d_usingflags(char *nbr, t_print *tab)
+{
+	if (tab->plus == 1)
+		nbr = ft_format_plus(nbr);
+	return (nbr);
+}
+
 int	ft_format_d(t_print *tab)
 {
-	int		d;
 	char	*nbr;
 
-	d = va_arg(tab->args, int);
-	nbr = ft_itoa(d);
+	nbr = ft_itoa(va_arg(tab->args, int));
+	if (!nbr)
+		return (0);
+	nbr = ft_format_d_usingflags(nbr, tab);
+	if (!nbr)
+		return (0);
 	ft_print_str((const char *)nbr, tab);
-	free(nbr);
-	return (0);
+	return (free(nbr), 0);
 }
 
 /*
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yrhiba <yrhiba@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 18:18:37 by yrhiba            #+#    #+#             */
-/*   Updated: 2022/11/01 01:51:30 by yrhiba           ###   ########.fr       */
+/*   Updated: 2022/11/01 02:21:10 by yrhiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int	main(void)
 	int	n1;
 	int	n2;
 
-	n1 = ft_printf("%d\n", 42);
-	n2 = printf("%d\n", 42);
+	n1 = ft_printf("%+d\n", 0);
+	n2 = printf("%+d\n", 0);
 	printf("|%d|\n", n1);
 	printf("|%d|\n", n2);
 }
